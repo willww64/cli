115fdd979b800ae81d2e7d546bfd7e0332dfb851
Will Wang <willww64@gmail.com>
2024-07-23 23:06:37 +0800

    Update cli/config/configfile/file.go
    use filepath.EvalSymlink instead of check with filepath.IsAbs
    
    Co-authored-by: Paweł Gronowski <me@woland.xyz>
    Signed-off-by: Will Wang <willww64@gmail.com>

--- cli/config/configfile/file.go
+++ cli/config/configfile/file.go
@@ -169,12 +169,8 @@ func (configFile *ConfigFile) Save() (retErr error) {
 
 	// Handle situation where the configfile is a symlink
 	cfgFile := configFile.Filename
-	if f, err := os.Readlink(cfgFile); err == nil {
+	if f, err := filepath.EvalSymlinks(cfgFile); err == nil {
 		cfgFile = f
-		// The target of a symlink can be a relative path, ensure the final path is absolute
-		if !filepath.IsAbs(f) {
-			cfgFile = filepath.Join(dir, f)
-		}
 	}
 
 	// Try copying the current config file (if any) ownership and permissions
